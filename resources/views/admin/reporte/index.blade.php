7a73249f0c2a82630def396fa37d4e8ad30b5ca7
danmendz <daniel.mdz33@gmail.com>
2024-04-01 18:23:21 -0600

    cambios en dashboard de admin

--- resources/views/admin/reporte/index.blade.php
+++ resources/views/admin/reporte/index.blade.php
@@ -32,36 +32,46 @@
             <!-- Content Row -->
             <div class="row">
 
-                <div class="col-xl-8 col-lg-7">
-
-                    <!-- Area Chart -->
+                <!-- DataTales Example -->
+                <div class="col-xl-7 col-lg-5">
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Uso</h6>
                         </div>
                         <div class="card-body">
-                            <div class="chart-area">
-                                <canvas id="myAreaChart"></canvas>
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Area</th>
+                                            <th>Pasillo</th>
+                                            <th>Cajon</th>
+                                            <th>Veces usado</th>
+                                            <th>% de uso</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>A</th>
+                                            <th>A1</th>
+                                            <th>5</th>
+                                            <th>6</th>
+                                            <th>45%</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <tr>
+                                            <td>C</td>
+                                            <td>C2</td>
+                                            <td>2</td>
+                                            <td>9</td>
+                                            <td>70%</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
-                            <hr>
-                            Cajones más usados en los eventos
                         </div>
                     </div>
-
-                    <!-- Bar Chart -->
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="chart-bar">
-                                <canvas id="myBarChart"></canvas>
-                            </div>
-                            <hr>
-                            Cajones más usados en los eventos
-                        </div>
-                    </div>
-
                 </div>
 
                 <!-- Donut Chart -->
@@ -69,7 +79,7 @@
                     <div class="card shadow mb-4">
                         <!-- Card Header - Dropdown -->
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Donut Chart</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Gráfica en dona</h6>
                         </div>
                         <!-- Card Body -->
                         <div class="card-body">
@@ -77,12 +87,68 @@
                                 <canvas id="myPieChart"></canvas>
                             </div>
                             <hr>
-                            Cajones más usados durante todo el mes.
+                            Cajones más utilizados durante los eventos.
                         </div>
                     </div>
                 </div>
 
+                <!-- DataTales Example -->
+                <div class="col-xl-7 col-lg-5">
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Disponibilidad</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Area</th>
+                                            <th>Pasillo</th>
+                                            <th>Cajon</th>
+                                            <th>% de disponibilidad</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>A</th>
+                                            <th>A2</th>
+                                            <th>5</th>
+                                            <th>60%</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <tr>
+                                            <td>B</td>
+                                            <td>B1</td>
+                                            <td>3</td>
+                                            <td>40%</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Bar Chart -->
+                <div class="col-xl-4 col-lg-5">
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Grafica de barras</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="chart-bar">
+                                <canvas id="myBarChart"></canvas>
+                            </div>
+                            <hr>
+                            Disponibilidad de los cajones de estacionamiento de una hora específica.
+                        </div>
+                    </div>
+                </div>                
+
             </div>
+
         </div>
         <!-- /.container-fluid -->
     </div>
