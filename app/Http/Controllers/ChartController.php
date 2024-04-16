792882a3bae5b96cb719b610aac59d3eab8c72c9
danmendz <daniel.mdz33@gmail.com>
2024-04-16 15:32:40 -0600

    chartcontroller

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -11,10 +11,11 @@ class ChartController extends Controller
     public function getData(Request $request)
     {
         $dia = $request->input('dia');
-        $hora = $request->input('hora');
+        $hora_inicio = $request->input('hora_inicio');
+        $hora_fin = $request->input('hora_fin');
 
         // Llamar al procedimiento almacenado y obtener el porcentaje de disponibilidad
-        $disponibilidad = DB::select('CALL obtener_disponibilidad(?, ?)', [$dia, $hora]);
+        $disponibilidad = DB::select('CALL obtener_disponibilidad(?, ?, ?)', [$dia, $hora_inicio, $hora_fin]);
 
         // Devolver los datos como una respuesta JSON
         return response()->json(['disponibilidad' => $disponibilidad]);
