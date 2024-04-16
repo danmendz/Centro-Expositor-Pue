7fc412b90929aea7c4d559a60b220cd5e3145ea8
danmendz <daniel.mdz33@gmail.com>
2024-04-15 23:53:31 -0600

    comand build

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -18,6 +18,9 @@
     <!-- Custom styles for this template-->
     <link href="{{asset('libs/sbadmin/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
+    {{-- <script data-main="js/app" src="js/lib/require.js"></script>
+    <script data-main="scripts/main" src="scripts/require.js"></script> --}}
+
 </head>
 
 <body id="page-top">
