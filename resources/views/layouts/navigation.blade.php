21963d0ff9a366e8881111f7b75887bce35d9fb9
danmendz <daniel.mdz33@gmail.com>
2024-04-16 21:41:50 -0600

    nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -490,6 +490,11 @@
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
+            <div class="mt-3 space-y-1">
+                <x-responsive-nav-link :href="route('reservar.evento')">
+                    {{ __('Reservar evento') }}
+                </x-responsive-nav-link>
+            </div>
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
--- resources/views/usuario/reserva/mis-reservas.blade.php
+++ resources/views/usuario/reserva/mis-reservas.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
     <div class="container mt-4">
+        <h1>Mis reservas</h1>
         @foreach($eventos as $evento)
             <div class="card mb-4">
                 <div class="card-body">
