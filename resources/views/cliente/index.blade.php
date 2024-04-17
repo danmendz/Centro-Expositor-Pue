250fceaf7755de2544744c0e106c1a4bf866536d
danmendz <daniel.mdz33@gmail.com>
2024-04-16 22:33:09 -0600

    images

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -4,7 +4,7 @@
             @foreach($eventos as $evento)
                 <div class="col-md-4 mb-4">
                     <div class="card bg-light mx-4">
-                        <img src="{{ asset('images/fiestas/drinks.jpg') }}" class="card-img-top" alt="Imagen del evento">
+                        <img src="{{ asset('images/fiestas/drinks.jpg') }}" width="70px" height="70px" class="card-img-top" alt="Imagen del evento">
                         <div class="card-body">
                             <h4 class="card-title"><strong>{{ $evento->nombre }}</strong></h4>
                             <p class="card-text"><strong>Fecha:</strong> {{ $evento->fecha_inicio }}</p>
