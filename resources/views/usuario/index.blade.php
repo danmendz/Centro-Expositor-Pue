d77c8916cfc2f3b6e8727923e0c1560e3914de2e
danmendz <daniel.mdz33@gmail.com>
2024-04-16 22:24:56 -0600

    events views

--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -2,10 +2,10 @@
     <div class="row">
         @foreach($allEventos as $evento)
             <div class="col-md-4 mb-4">
-                <div class="card bg-light">
-                    <img src="{{ asset('images/globos.jpg') }}" class="card-img-top" alt="Imagen del evento">
+                <div class="card bg-light mx-4">
+                    <img src="{{ asset('images/fiestas/globos.jpg') }}" class="card-img-top" alt="Imagen del evento">
                     <div class="card-body">
-                        <h5 class="card-title text-primary">{{ $evento->nombre }}</h5>
+                        <h4 class="card-title"><strong>{{ $evento->nombre }}</strong></h4>
                         <p class="card-text"><strong>Fecha:</strong> {{ $evento->fecha_inicio }}</p>
                         <p class="card-text"><strong>Tipo:</strong> {{ $evento->tipo }}</p>
                         <p class="card-text"><strong>Organizador:</strong> {{ $evento->user->name }}</p>
